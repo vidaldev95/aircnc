0ca312c7a6e044830fb4cf258d7e1b62f4a8a5ed
lenda95 <vidalcg95@gmail.com>
2019-10-25 03:02:13 -0300

    Finalizando desenvolvimento com ReactJS para a aplicação versão Web, consumindo API em NodeJS.
    Ending development with ReactJS for web version application, consuming NodeJS API.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 const app = express ();
 
@@ -11,7 +14,9 @@ mongoose.connect('mongodb+srv://user:swordfish95@aircnc-d9csn.mongodb.net/aircnc
 //req.params = Acessar route params(para edição, delete)
 //req.body = Acessar corpo da requisição (para criação, edição...)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
